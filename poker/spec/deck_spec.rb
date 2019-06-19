7b5bcd0add92dfe1008f5f023e39819e1e5010f0
kanuff <makerman4@gmail.com>
2019-06-19 17:28:29 -0400

    refactor RSpec

--- poker/spec/deck_spec.rb
+++ poker/spec/deck_spec.rb
@@ -10,45 +10,11 @@ describe Deck do
     end
   end
 
-
-  describe ("#populate") do
-    it "should return an array of card objects" do
-      deck.populate 
-      expect(deck.deck[0]).to be_an_instance_of(Card)
-    end
-
-    it "should return an array with length 52" do
-      deck.populate 
-      expect(deck.deck.length).to eq(52)
-    end
-
-    # it "calls #suits 4 times" do
-    #   expect(deck).to receive(:suits).exactly(4).times
-    # end
-
-    # it "calls #symbols 4 times" do
-    #   expect(deck).to receive(:symbols).exactly(4).times
-    # end
-
-  end
-
-  describe ("#full_deck?") do 
-    it "should return true if the deck has 52 unique cards" do
-      deck.populate
-      expect(deck.full_deck?).to eq(true)
-    end
-  end
-
-  describe "#shuffle_deck" do
-    it "should randomize the deck order" do
-      deck.populate
-      old_deck = deck.deck.dup
-      deck.shuffle_deck
-      expect(deck.deck).to_not eq(old_deck)
-    end
-  end
-
   describe ("#ready_the_deck") do
+    after(:each) do
+      deck.ready_the_deck
+    end
+
     it "should call #populate" do
       expect(deck).to receive(:populate)
     end
@@ -59,6 +25,36 @@ describe Deck do
 
     it "should call #shuffle_deck" do
       expect(deck).to receive(:shuffle_deck)
+    end
+  end
+
+  context "when preparing decks" do
+    before(:each) do 
+      deck.populate
+    end
+
+    describe ("#populate") do
+      it "should return an array of card objects" do
+        expect(deck.deck[0]).to be_an_instance_of(Card)
+      end
+
+      it "should return an array with length 52" do
+        expect(deck.deck.length).to eq(52)
+      end
+    end
+
+    describe ("#full_deck?") do 
+      it "should return true if the deck has 52 unique cards" do
+        expect(deck.full_deck?).to eq(true)
+      end
+    end
+
+    describe "#shuffle_deck" do
+      it "should randomize the deck order" do
+        old_deck = deck.deck.dup
+        deck.shuffle_deck
+        expect(deck.deck).to_not eq(old_deck)
+      end
     end
   end
 
